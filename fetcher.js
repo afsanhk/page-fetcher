7101d22c48bf54d9ae89b0b64dbaf89d77e534f7
Afsanul Haque Khan <afsanhk@gmail.com>
2021-06-30 14:46:57 +0000

    Changed comments to make the flow of code more understandable

--- fetcher.js
+++ fetcher.js
@@ -1,3 +1,4 @@
+// Import file system, request and readline. 
 const request = require('request');
 const fs = require('fs');
 const readline = require('readline');
@@ -12,14 +13,15 @@ const args = process.argv.slice(2);
 const url = args[0];
 const path = args[1];
 
-// EDGE CASE: If the path exists, initaite a readline, if y + enter, then execute rest of code, otherwise end.
-// fs.access only returns an error if the file does NOT exist. So if no error, then the file exists!
+// EDGE CASE: If the path exists, initiate a readline, if y + enter, then execute rest of code, otherwise end.
+//            If the path doesn't exist, just execute the code. 
+// NOTE: fs.access only returns an error if the file does NOT exist. So if no error, then the file exists!
 fs.access(path, fs.constants.F_OK, (err) => {
   if (!err) {
     console.log(`File exists!`);
     rl.question(`Would you like to overwrite the existing file? Press y, followed by enter for yes. \n> `, answer => {
       if (answer === 'y') {
-        // It should download the resource at the URL
+        // It should request the resource at the URL
         request(url, (error, response, body) => {
           // EDGE CASE: Return an error if the URL is invalid. 
           if(error) {
@@ -27,7 +29,7 @@ fs.access(path, fs.constants.F_OK, (err) => {
             return error;
           } 
         
-          // To the local path on your machine
+          // Write to the local path on your machine
           fs.writeFile(path, body, err => {
             if (err) {
               console.log(`File path is invalid \n ${err}`)
