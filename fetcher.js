b9687d97fb77ed8b5fe12beeb3967687f607cecf
Afsanul Haque Khan <afsanhk@gmail.com>
2021-06-30 01:02:06 +0000

    Updated fetcher.js to include two edge cases 1)Return errors for invalid URLs using requests in-built err 2)Used fs.access to assess whether a file exists, then used user prompts to overwrite existing file or not.

--- fetcher.js
+++ fetcher.js
@@ -12,33 +12,48 @@ const args = process.argv.slice(2);
 const url = args[0];
 const path = args[1];
 
-// If the path exists, return an error:
+// EDGE CASE: If the path exists, initaite a readline, if y + enter, then execute rest of code, otherwise end.
+// fs.access only returns an error if the file does NOT exist. So if no error, then the file exists!
+fs.access(path, fs.constants.F_OK, (err) => {
+  if (!err) {
+    console.log(`File exists!`);
+    rl.question(`Would you like to overwrite the existing file? Press y, followed by enter for yes. \n> `, answer => {
+      if (answer === 'y') {
+        // It should download the resource at the URL
+        request(url, (error, response, body) => {
+          // EDGE CASE: Return an error if the URL is invalid. 
+          if(error) {
+            console.log(`URL is invalid`)
+            return error;
+          } 
+        
+          // To the local path on your machine
+          fs.writeFile(path, body, err => {
+            if (err) {
+              console.log(`File path is invalid`)
+              return err;
+            }
+            // It should print out a message after completion of writing
+            return console.log(`Downloaded and saved ${body.length} bytes to ${path}`)          
+          })
+        });
+      } rl.close()
+    })
+    // If the file doesn't exist, just download the file:
+  } else {
+      request(url, (error, response, body) => {
+      if(error) {
+        console.log(`URL is invalid`)
+        return error;
+      } 
 
-
-// It should download the resource at the URL
-request(url, (error, response, body) => {
-  if(error) {
-    console.log(`URL is invalid`)
-    return error;
-  } 
-  console.log('statusCode:', response && response.statusCode); 
-  console.log('body:', body); 
-  console.log('body length: : ', body.length);
-
-  // To the local path on your machine
-  fs.writeFile(path, body, err => {
-    if (err) {
-      console.log(`File path is invalid`)
-      return err;
-    }
-    // It should print out a message after completion of writing
-    console.log(`Downloaded and saved ${body.length} bytes to ${path}`)
-    
-  })
-
-});
-
-
-
-
-
+      fs.writeFile(path, body, err => {
+        if (err) {
+          console.log(`File path is invalid`)
+          return err;
+        }
+        console.log(`Downloaded and saved ${body.length} bytes to ${path}`);
+        process.exit()          
+      })
+  })}  
+})
